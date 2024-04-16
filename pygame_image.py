6d7d96d77af1bec00f8f0e0fc7a223f4aa1fec5a
masaya <c0a23086a8@edu.teu.ac.jp>
2024-04-16 17:52:40 +0900

    演習1,2

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,31 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     bg2_img = pg.transform.flip(bg_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        y = 0
+        z = 0
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            y = -1
+        if key_lst[pg.K_DOWN]:
+            y = 1
+        if key_lst[pg.K_LEFT]:
+            z = -1
+        if key_lst[pg.K_RIGHT]:
+            z = 2
+        kk_rct.move_ip([-1+z, y])  
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg2_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg2_img, [-x+4800, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1      
         clock.tick(200)
